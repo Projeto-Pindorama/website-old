29c77aacaab7239caa7484e6b938eb3cd29d00cb
Luiz Antônio <luiz.antonio.rangel@bol.com.br>
2021-04-15 16:44:41 -0300

    Backup do servidor às Thu Apr 15 16:44:41 -03 2021.

--- scripts/run_thttpd.sh
+++ scripts/run_thttpd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 [ "`whoami`" != 'root' ] && { echo 'Run it with root, idiot.'; exit 1; }
-exec /bin/thttpd -dd /usr/storage0/www -nor -t /usr/storage0/www/thttpd/throttle.conf -l /var/log/thttpd.log &
+exec /bin/thttpd -dd /usr/str0/www -nor -t /usr/str0/www/thttpd/throttle.conf -l /var/log/thttpd.log &
